e48d95e610f1bb61b6753df7a551c44bfa7d3afd
Gisela <gngariboldi@gmail.com>
2022-03-29 17:32:50 -0300

    subiendo archivos

---
+++ index.js
@@ -0,0 +1,6 @@
+var num1 = parseInt(prompt("Ingrese el primer número: "))
+var num2 = prompt("Ingrese el segundo número: ")
+var suma = num1 + parseInt(num2);
+var resta = num1 - num2;
+var producto = num1 * num2;
+var division = num2/ num1;
